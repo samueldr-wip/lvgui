f703e634674d4e7fb09386e0c00048be9640b06c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-06-26 21:26:33 -0400

    build.nix: sync with Mobile NixOS
    ... and fix build

--- build.nix
+++ build.nix
@@ -1,11 +1,15 @@
 { stdenv
 , lib
-, fetchFromGitHub
+, nix-gitignore
+
 , pkg-config
 , libevdev
 , libxkbcommon
-, nix-gitignore
+, freetype
 , SDL2
+, libdrm
+, libinput
+
 , withSimulator ? false
 }:
 
@@ -21,12 +25,22 @@ in
 
     src = nix-gitignore.gitignoreSource [] ./.;
 
+    # Document `LVGL_ENV_SIMULATOR` in the built headers.
+    # This allows the mrbgem to know about it.
+    # (In reality this should be part of a ./configure step or something similar.)
+    postPatch = ''
+      sed -i"" '/^#define LV_CONF_H/a #define LVGL_ENV_SIMULATOR ${if withSimulator then "1" else "0"}' lv_conf.h
+    '';
+
     nativeBuildInputs = [
       pkg-config
     ];
 
     buildInputs = [
+      freetype
       libevdev
+      libdrm
+      libinput
       libxkbcommon
     ]
     ++ optionals withSimulator simulatorDeps
@@ -37,30 +51,11 @@ in
     ];
 
     makeFlags = [
+      "PREFIX=${placeholder "out"}"
     ]
     ++ optional withSimulator "LVGL_ENV_SIMULATOR=1"
     ++ optional (!withSimulator) "LVGL_ENV_SIMULATOR=0"
     ;
-
-    # TODO: `make install`...
-    installPhase = ''
-      mkdir -p $out/lib
-      cp -vr lib*.a $out/lib/
-
-      mkdir -p $out/include
-      find . -name '*.h' -exec install -vD '{}' $out/include/'{}' ';'
-
-      mkdir -p $out/lib/pkgconfig
-      cat <<EOF > $out/lib/pkgconfig/lvgui.pc
-      Name: lvgui
-      Description: LVGL-based GUI library
-      Version: $version
-      Requires: ${optionalString withSimulator "sdl2"}
-
-      Cflags: -I$out/include
-      Libs: $out/lib/liblvgui.a
-      EOF
-    '';
 
     enableParallelBuilding = true;
   }
